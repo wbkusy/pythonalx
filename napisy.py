558bc9bd4f97b64f338894b52a5cb022d2c422af
wbkusy <wbkusy@o2.pl>
2019-10-21 20:33:09 +0200

    Zmiany w skryptach

--- napisy.py
+++ napisy.py
@@ -9,3 +9,20 @@ napis5 = "znaki specjalne \t \n \r "
 
 dlugosc = len(napis1)
 print("Długość zmiennej napis to ",dlugosc,"znaków")
+# wiek = input("podaj wiek:")
+# print("twój wiek to: ",wiek.strip())
+
+s = "Ruda tańczy jak szalona"
+print(s.capitalize()) # kapitalki
+print(s.upper())      # góra
+print(s.lower())      # dół
+print(s.title())      # formatowanie jako tytuł
+print(s.swapcase())   # duże w małe, małe w duże
+print(s.center(100))  # centrowanie
+print(s.replace("R","D")) # zamina znaków
+print("czy liczba:",s.isdecimal()) # sprawdzenie czy string jest liczbą
+
+litera = s[3] # pobierz czwartą literę napisu
+print("4-ta litera:", s[3])
+print("litera przedostatnia", s[-2])
+
---
+++ string_slice.py
@@ -0,0 +1,12 @@
+d = "hello!"
+print(d[0:5]) # od indeksu 0 do indeksu 5
+print(d[-3])
+
+s = "Ruda tańczy jak szalona"
+arr = s.split(" ")
+print(arr[0]) # pierwszy element listy
+print(s[0:16:2]) # co 2 znak od 0 do 15 włącznie
+print(s[::3]) # cały string co 3 znak
+
+# grande fianl# reverse w pythonie
+print(s[::-1])
