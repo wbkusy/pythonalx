8ee84f13a06fdd133c01056396a2d17b1a0cf3cd
wbkusy <wbkusy@o2.pl>
2019-10-21 19:50:48 +0200

    Zmiany w skryptach

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,5 @@
 #
 #
 print("Ruda tańczy jak szalona")
+a = 123.45
+print(a)
---
+++ napisy.py
@@ -0,0 +1,11 @@
+#
+# Typ danych: napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'a to jest "cudzysłów" '
+napis4 = "a to jest \"cudzysłów\" "
+napis5 = "znaki specjalne \t \n \r "
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis to ",dlugosc,"znaków")
