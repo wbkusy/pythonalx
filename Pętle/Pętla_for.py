195a8b38438c74a8882f5e2ffda9df1d3b722659
wbkusy <wbkusy@o2.pl>
2019-10-30 16:19:02 +0100

    tabliczka mnożenia

--- Pętle/Pętla_for.py
+++ Pętle/Pętla_for.py
@@ -51,3 +51,13 @@ for i in range(5):
     print(i, end="")
 print()
 print(1,  2,  3,  4, sep="-" )
+
+print("   ", end="")
+for i in range(10):
+    print(f"{i:3}", end="")
+print()
+for i in range(10):
+    print(i, end="  ")
+    for j in range(10):
+        print(f"{i*j:3}", end="")
+    print()
