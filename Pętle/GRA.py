d27f371a12e9a310b0a39a3f9dbdbecf0095881c
wbkusy <wbkusy@o2.pl>
2019-11-01 11:26:33 +0100

    wizualizacja planszy

--- Pętle/GRA.py
+++ Pętle/GRA.py
@@ -1,10 +1,10 @@
 import random
 
 # w zależności od zmiennej DEBUG printuje położenie skarbu
-x1 = random.randint(1, 10)
-x2 = random.randint(1, 10)
-y1 = random.randint(1, 10)
-y2 = random.randint(1, 10)
+x1 = random.randint(0, 9)
+x2 = random.randint(0, 9)
+y1 = random.randint(0, 9)
+y2 = random.randint(0, 9)
 
 odleglosc_po_wylosowaniu = abs(x2 - x1) + abs(y2 - y1)
 z2 = x2
@@ -26,8 +26,16 @@ Uważaj! możesz wypaść poza planszę.
 po każdym ruchu możesz dostać informację o położeniu!""")
 s = 0
 while True:
-
-    R =  random.randint(1, 5)
+    print("Mapa:")
+    for i in range(10):
+        print()
+        for j in range(10):
+            if i == y1 and j == x1:
+                print("G", end="  ")
+            else:
+                print("Y", end="  ")
+    print()
+    R = random.randint(1, 5)
     if R == 1:
         DEBUG = True
     else:
@@ -38,9 +46,9 @@ while True:
             print("odległość od skarbu:", abs(x2 - x1) + abs(y2 - y1))
 
     ruch = input("podaj komendę: ")  # pyta gracza o ruch inputem
-    if ruch == 'w':  # zmieniamy pozycję gracza - zgodnie z komendą
+    if ruch == 's':  # zmieniamy pozycję gracza - zgodnie z komendą
         y1 = y1 + 1
-    elif ruch == 's':
+    elif ruch == 'w':
         y1 = y1 - 1
     elif ruch == 'a':
         x1 = x1 - 1
@@ -51,7 +59,7 @@ while True:
 
     if DEBUG:
         print(f"położenie gracza po ruchu: (x={x1}, y={y1})")
-    if not (1 <= x1 <= 10 and 1 <= y1 <= 10):
+    if not (0 <= x1 <= 9 and 0 <= y1 <= 9):
         print("wypadłeś poza planszę")
         break
     odleglosc_po_ruchu = abs(x2 - x1) + abs(y2 - y1)
@@ -71,16 +79,14 @@ while True:
     odleglosc_po_wylosowaniu = odleglosc_po_ruchu
     s = s + 1
 
-    if s> 2*abs(z2 - b1) + abs(c2 - n1):
+    if s > 2 * abs(z2 - b1) + abs(c2 - n1):
         print("wykorzystałeś max liczbę ruchów")
-        print(2*(abs(z2 - b1) + abs(c2 - n1)))
-        y1 = random.randint(1, 10)
-        y2 = random.randint(1, 10)
+        print(2 * (abs(z2 - b1) + abs(c2 - n1)))
+        y1 = random.randint(0, 9)
+        y2 = random.randint(0, 9)
         print("zmiana położenia sparbu")
         n1 = y1
         c2 = y2
-        s=0
+        s = 0
 
-
-
-# Postaraj się zwizualizowac położeneie gracza na planszy (AlA tabliczka mnożenia
+# Postaraj się zwizualizowac położeneie gracza na planszy (AlA tabliczka mnożenia)
