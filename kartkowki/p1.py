0dd456714fd2e7c92e6e39060ddfbe247ea8efba
wbkusy <wbkusy@o2.pl>
2019-11-06 20:31:39 +0100

    testy ze slownikow

---
+++ kartkowki/p1.py
@@ -0,0 +1,33 @@
+# podaj typy danych
+
+#str
+#int
+#bool
+#decimal
+#float
+#complex
+#none
+
+
+# podaj podstawowe struktury danych
+
+#tuple
+#list
+#dict
+#set
+
+#  podaj dwie metody sortowania listy
+
+# list.sort()
+# sorted(list)
+
+# podaj jak wywołać coś ze słownika
+
+# slownik.get('ala')  -  zwraca none jeśłi nie ma klucza
+
+
+# napisz funkcję która połączy dwa teksty a potem je odwróci
+
+# def polacz(x, z):
+#     u=x+z
+#     return u[::-1]
