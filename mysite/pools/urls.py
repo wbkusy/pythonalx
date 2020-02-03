7e0032d607628652cd9d6138c080d85202c5bf4b
wbkusy <wbkusy@o2.pl>
2020-02-03 20:29:13 +0100

    django strona www

--- mysite/pools/urls.py
+++ mysite/pools/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from pools.views import funkcja_widoku, hello_name, operacje, index, Question_views
+from pools.views import funkcja_widoku, hello_name, operacje, index, question_details, question_list, vote
 
+
+app_name = "pools"
 urlpatterns = [
-    path('question/', Question_views),
-    #path("", index, name="index"),
+    path("", index, name="index"),
     path('hello/', funkcja_widoku),
     path('hello/<name>', hello_name),
-    path('<op>/<a>/<b>', operacje, name="question-list"),
-    path('q/<nr>', Question_views)
+    path('<op>/<a>/<b>', operacje),
+    path('questions/', question_list, name="list"),
+    path('questions/<id>', question_details, name="details"),
+    path('pytnaie/<id>/vote', vote, name="vote"),
 ]
--- mysite/pools/views.py
+++ mysite/pools/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from pools.models import Question
 
@@ -27,6 +28,7 @@ def hello_name(request, name):
 
 # Stwórz kalkulator
 
+
 def operacje(request, op, a, b):
     if op == "sum":
         return HttpResponse(int(a)+int(b))
@@ -38,18 +40,40 @@ def operacje(request, op, a, b):
         return HttpResponse(int(a)/int(b))
 
 
-def Question_views(request, nr):
-    quest = Question.objects.all()
-    text = ""
-    for i in quest:
-        if i == int(nr):
-            text = f"{quest(i)}"
-    return HttpResponse(text)
+def question_list(request):
+    lista = Question.objects.all()
+    return render(
+        request,
+        "list.html",
+        {"questions": lista}
+    )
+
+
+def question_details(request, id):
+    quest = Question.objects.get(pk=id)
+    return render(
+        request,
+        "details.html",
+        {"question": quest}
+    )
 
 
 def index(request):
     return HttpResponse("Hello world! That's pools index")
 
+
+def vote(request, id):
+    question = Question.object.get(pk=id)
+    choice_id = request.POST.get("choice")
+    if choice_id:
+        choice = question.choices.get(id=choice_id)
+        choice.votes += 1
+        choice.save()
+    else:
+        return render(request, "details.html", {"question": question,
+                                                "error": "nie wybrales odpowiedzi"})
+    print("odbylo sie glosowanie")
+    return HttpResponseRedirect(reverse("pools:list"))
 # funkcja widok szczegółów
 # np question/1 djangoproject.com/en/3.0/intro/tutorial03/
 # python principles.com -> challanges
